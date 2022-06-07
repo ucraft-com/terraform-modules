e26c5a286c9642ec2f0727499d68d2ea5a4bf382
devops-mher <mher.poghosyan@ucraft.com>
2022-06-07 14:22:46 +0400

    fix module vm

--- compute_instance/outputs.tf
+++ compute_instance/outputs.tf
@@ -1,9 +1,4 @@
-output "zones_available" {
-  description = "Output gc zones having free space."
-  value       = "{data.google_compute_zones.available.names}"
-}
-
-output "addresses" {
-  description = "VM IP addresses."
-  value       = join(",", google_compute_address.instances.*.address)
+output "instance_id" {
+  description = "The gce instance id"
+  value       = google_compute_instance.default.instance_id
 }

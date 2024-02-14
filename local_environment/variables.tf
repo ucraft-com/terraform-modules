d2b8a4d26e4950041742ae71a6f5ea4658725683
Mher Poghosyan <mher.poghosyan@ucraft.com>
2024-02-14 17:53:33 +0400

    add new prefix

--- local_environment/variables.tf
+++ local_environment/variables.tf
@@ -120,5 +120,5 @@ variable "valid_suffixes_supervisor" {
 variable "valid_prefixes_supervisor" {
   description = "List of valid suffixes for server_host"
   type        = list(string)
-  default     = ["localhost", "host.docker.internal", "172.16.11.4"]
+  default     = ["localhost", "host.docker.internal", "172.16.11.4", "172.16.11.5"]
 }

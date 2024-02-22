07e46b10d4a9079ef598bff371e2fd86b93e0614
Mher Poghosyan <mher.poghosyan@ucraft.com>
2024-02-22 09:36:42 +0400

    update secret manager module

--- data_secret_manager/main.tf
+++ data_secret_manager/main.tf
@@ -1,7 +1,15 @@
 data "google_secret_manager_secret_version" "this" {
   secret = var.secret
 }
+data "template_file" "env" {
+  template = data.google_secret_manager_secret_version.this.secret_data
+
+  vars = {
+    protocol = var.protocol
+    domain   = var.domain
+  }
+}
 resource "local_file" "private_key" {
-  content  = data.google_secret_manager_secret_version.this.secret_data
+  content  = data.template_file.env.rendered
   filename = var.filename
 }

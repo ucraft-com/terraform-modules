23383e6c2c886cc377084eb7dab54c64f80eaf36
Mher Poghosyan <mher.poghosyan@ucraft.com>
2024-03-01 02:56:56 +0400

    update template module

--- templates/main.tf
+++ templates/main.tf
@@ -8,6 +8,17 @@ resource "null_resource" "install_templates" {
   }
 }
 
+resource "null_resource" "download_script" {
+  provisioner "local-exec" {
+    command = "curl -o get_latest_migration_version.py https://raw.githubusercontent.com/ucraft-com/terraform-modules/master/templates/get_latest_migration_version.py"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
 data "external" "get_latest_migration_version" {
-  program = ["python3", "get_latest_migration_version.py"]
+  depends_on = [null_resource.download_script]
+  program = ["python3", "${path.module}/get_latest_migration_version.py"]
 }

69923c2e9104324316d20ae35987ceeec9ac3f93
Gurgen Yegoryan <gurgen.y@ucraft.com>
2024-08-09 20:07:07 +0400

    Disable templates

--- templates/main.tf
+++ templates/main.tf
@@ -1,12 +1,12 @@
-resource "null_resource" "install_templates" {
-  for_each = toset(var.templates)
-  provisioner "local-exec" {
-    command = <<-EOF
-      LATEST_FOLDER=$(gsutil ls gs://templates-uc-next/ | grep -E "^.*/[0-9]+(\\.[0-9]+){0,2}/" | sort | tail -n 1)
-      gsutil cp "$${LATEST_FOLDER}${each.key}.tar.gz" data_volumes/static/templates
-    EOF
-  }
-}
+# resource "null_resource" "install_templates" {
+#   for_each = toset(var.templates)
+#   provisioner "local-exec" {
+#     command = <<-EOF
+#       LATEST_FOLDER=$(gsutil ls gs://templates-uc-next/ | grep -E "^.*/[0-9]+(\\.[0-9]+){0,2}/" | sort | tail -n 1)
+#       gsutil cp "$${LATEST_FOLDER}${each.key}.tar.gz" data_volumes/static/templates
+#     EOF
+#   }
+# }
 
 resource "null_resource" "download_script" {
   provisioner "local-exec" {
